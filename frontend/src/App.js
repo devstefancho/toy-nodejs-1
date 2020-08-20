33a9443e185341bf1297095d2f0fb67f8af77652
skku-sungjin <skku7714@gmail.com>
2020-08-20 22:39:07 +0900

    default route with nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Blogs } from "./components/blogs";
+import { About } from "./components/about";
+import { Create } from "./components/create";
+import { Nav } from "./common/nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello World</header>
-    </div>
+    <>
+      <Nav></Nav>
+      <Router>
+        <div>
+          <Route path="/blogs">
+            <Blogs></Blogs>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/create">
+            <Create></Create>
+          </Route>
+          <Route exact path="/">
+            Hello
+          </Route>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ frontend/src/common/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>Blogs</li>
+        <li>About</li>
+        <li>Create</li>
+      </ul>
+    </div>
+  );
+};
