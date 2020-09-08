9a20f391c3b16071040ef93240983a56428c1cd3
skku-sungjin <skku7714@gmail.com>
2020-09-08 23:37:16 +0900

    option name, state output
    confirm category option name
    confirm console output of target state

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -2,67 +2,110 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { useEffect } from "react";
 
 export const Create = () => {
   const [isloading, setIsloading] = useState(false);
+  const [title, setTitle] = useState("");
   const [firstOption, setFirstOption] = useState("language");
+  const [secondOption, setSecondOption] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    console.log("=================");
+    console.log(title);
+    console.log(firstOption);
+    console.log(secondOption);
+    console.log(description);
+  }, [title, firstOption, secondOption, description]);
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const onChangeFirstOption = (e) => {
+    setFirstOption(e.target.value);
+  };
+
+  const onChangeSecondOption = (e) => {
+    setSecondOption(e.target.value);
+  };
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
   const handleClick = () => setIsloading(true);
   return (
     <>
       <Form>
+        {/*title input*/}
         <Form.Row>
           <Form.Group as={Col} md="4" controlId="formGridTitle">
             <Form.Label>Title</Form.Label>
-            <Form.Control required type="text" placeholder="Enter Title" />
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} md="4" controlId="formGridCategory">
-            <Form.Label>Category</Form.Label>
             <Form.Control
-              as="select"
-              onChange={(e) => setFirstOption(e.target.value)}
-            >
-              <option value="language">language</option>
-              <option value="ideas">ideas</option>
-              <option value="news">news on tech</option>
-              <option value="useful-sitemap">useful sitemap</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group as={Col} md="4" controlId="formGridSubCategory">
-            <Form.Label>Sub-Category</Form.Label>
-            <Form.Control as="select">
-              {firstOption && firstOption === "language" && (
-                <>
-                  <option value="">Javascript</option>
-                  <option value="">HTML5</option>
-                  <option value="">CSS3</option>
-                </>
-              )}
-              {firstOption && firstOption === "ideas" && (
-                <>
-                  <option value="">Business Idea</option>
-                  <option value="">Application</option>
-                  <option value="">Services</option>
-                </>
-              )}
-              {firstOption && firstOption === "news" && (
-                <>
-                  <option value="">a</option>
-                  <option value="">b</option>
-                  <option value="">c</option>
-                </>
-              )}
-              {firstOption && firstOption === "useful-sitemap" && (
-                <>
-                  <option value="">1</option>
-                  <option value="">2</option>
-                  <option value="">3</option>
-                </>
-              )}
-            </Form.Control>
+              required
+              type="text"
+              placeholder="Enter Title"
+              onChange={onChangeTitle}
+            />
           </Form.Group>
         </Form.Row>
+        {/*firstOption and secondOption selection*/}
+        <Form.Row>
+          <Form.Group as={Col} md="4" controlId="formGridFirstOption">
+            <Form.Label>First Category</Form.Label>
+            <Form.Control as="select" onChange={onChangeFirstOption}>
+              <option value="language">Language</option>
+              <option value="idea">Idea</option>
+              <option value="book">Book</option>
+              <option value="bookmark">Bookmark Link</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="formGridSecondOption">
+            <Form.Label>Second Category</Form.Label>
+            {firstOption && firstOption === "language" && (
+              <Form.Control as="select" onChange={onChangeSecondOption}>
+                <option value="" selected="selected" disabled>
+                  select one...
+                </option>
+                <option value="language-javascript">Javascript</option>
+                <option value="language-html5">HTML5</option>
+                <option value="language-css3">CSS3</option>
+              </Form.Control>
+            )}
+            {firstOption && firstOption === "idea" && (
+              <Form.Control as="select" onChange={onChangeSecondOption}>
+                <option value="" selected="selected" disabled>
+                  select one...
+                </option>
+                <option value="idea-business ">Business Idea</option>
+                <option value="idea-application">Application</option>
+                <option value="idea-service">Services</option>
+              </Form.Control>
+            )}
+            {firstOption && firstOption === "book" && (
+              <Form.Control as="select" onChange={onChangeSecondOption}>
+                <option value="" selected="selected" disabled>
+                  select one...
+                </option>
+                <option value="book-list">book list</option>
+                <option value="book-quotes">inspirational quotes</option>
+                <option value="book-misc">Misc</option>
+              </Form.Control>
+            )}
+            {firstOption && firstOption === "bookmark" && (
+              <Form.Control as="select" onChange={onChangeSecondOption}>
+                <option value="" selected="selected" disabled>
+                  select one...
+                </option>
+                <option value="bookmark-youtube">Youtube</option>
+                <option value="bookmark-article">Article</option>
+                <option value="bookmark-misc">Misc</option>
+              </Form.Control>
+            )}
+          </Form.Group>
+        </Form.Row>
+        {/*description(text-area)*/}
         <Form.Row>
           <Form.Group as={Col} md="8" controlId="formGridDescription">
             <Form.Label>Description</Form.Label>
@@ -70,23 +113,25 @@ export const Create = () => {
               as="textarea"
               // @ts-ignore
               rows="10"
+              onChange={onChangeDescription}
             />
           </Form.Group>
         </Form.Row>
+        {/*submit button*/}
+        <Form.Row>
+          <Form.Group as={Col} md="8" className="flex-row-reverse">
+            <div className="text-right">
+              <Button
+                variant="primary"
+                disabled={isloading}
+                onClick={!isloading ? handleClick : null}
+              >
+                Submit
+              </Button>{" "}
+            </div>
+          </Form.Group>
+        </Form.Row>
       </Form>
-      <Form.Row>
-        <Form.Group as={Col} md="8" className="flex-row-reverse">
-          <div className="text-right">
-            <Button
-              variant="primary"
-              disabled={isloading}
-              onClick={!isloading ? handleClick : null}
-            >
-              Submit
-            </Button>{" "}
-          </div>
-        </Form.Group>
-      </Form.Row>
     </>
   );
 };
